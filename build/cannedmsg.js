55bafa31178b8eef337a29ca0bcedb74797b811e
sachindongare029 <sachindongare029@gmail.com>
2020-07-20 13:32:45 +0530

    changes

--- build/cannedmsg.js
+++ build/cannedmsg.js
@@ -19569,20 +19569,40 @@ App.views.ResultView = Backbone.View.extend({
 
   events: {},
 
-  initialize: function() {
+  initialize: function () {
     _.bindAll(this, "render");
     this.render();
   },
 
-  render: function() {
+  render: function () {
+    var demoJson = [
+      {
+        name: "Brand Logo",
+        createdBy: "Me",
+        status: "Active",
+        actions: ["view", "edit"],
+      },
+      {
+        name: "Brand Marketing Visuals",
+        createdBy: "Mikimoto",
+        status: "Active",
+        actions: ["edit"],
+      },
+      {
+        name: "Brand Bio",
+        createdBy: "Matt",
+        status: "Inactive",
+        actions: ["view"],
+      },
+    ];
     var self = this;
-    $.get("/src/templates/resultView.hbs", function(templateHtml) {
+    $.get("/src/templates/resultView.hbs", function (templateHtml) {
       var template = Handlebars.compile(templateHtml);
       var finalHtml = template();
-      self.$el.html(finalHtml);
+      self.$el.html(finalHtml(demoJson));
     });
     return self;
-  }
+  },
 });
 ;var App = App || {};
 
